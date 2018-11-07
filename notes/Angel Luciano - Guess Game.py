940da3c17862ae4e9f5ea0e18d0c43fa3cbeb4b2
AngelLuciano <42391635+AngelLuciano@users.noreply.github.com>
2018-11-07 14:34:29 -0800

    Almost finished with guess games

--- notes/Angel Luciano - Guess Game.py
+++ notes/Angel Luciano - Guess Game.py
@@ -1,3 +1,27 @@
-import  random
-print(random.radiant(0, 10))
-def  random(percentage)
+import random
+
+guessesTaken = 0
+
+playing = True
+
+number = random.randint(1, 10)
+
+while guessesTaken < 5:
+    print("Guess the number")
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken += 1
+
+    if guess < number:
+        print("Your number guess is low")
+
+    if guess > number:
+        print("Your number is high.")
+
+    if guess == number:
+        playing = False
+        print("Good job you guessed the right number")
+
+    if guess != number:
+        print("You Lost")
