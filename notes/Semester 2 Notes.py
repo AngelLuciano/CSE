41c888efa7dc9266405c2f4eb4d07062ba0ef849
AngelLuciano <42391635+AngelLuciano@users.noreply.github.com>
2019-01-09 14:08:29 -0800

    review

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,15 @@
+print("Hello world")
+# hi#
+cars = 5
+driving = True
+print("I have %s cars" % cars)
+age = input("How old are you?")
+print("%s?? Really???" % age)
+color_list = ['blue', 'red', 'yellow', 'black']
+color_list.append('cyan')
+
+print(color_list)
+color_list.pop(0)
+print(color_list)
+print(color_list[1])
+print(len(color_list))
